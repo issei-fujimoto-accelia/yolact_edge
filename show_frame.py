d53b5d02f932eaa5a6eca5c18b3a5a48b299af4c
i.fujimoto <i.fujimoto@accelia.net>
2023-10-05 17:32:20 +0900

    add dot

--- show_frame.py
+++ show_frame.py
@@ -131,7 +131,7 @@ def prep_display(args, cfg, dets_out, img, h, w, undo_transform=True, class_colo
         for j in reversed(range(num_dets_to_consider)):
             x1, y1, x2, y2 = boxes[j, :]
             color = get_color(j)
-            score = scores[j]
+            score = scores[j]         
 
             if args.display_bboxes:
                 cv2.rectangle(img_numpy, (x1, y1), (x2, y2), color, 1)
@@ -149,6 +149,18 @@ def prep_display(args, cfg, dets_out, img, h, w, undo_transform=True, class_colo
                 text_color = [0, 0, 0]
                 
                 cv2.putText(img_numpy, text_str, text_pt, font_face, font_scale, text_color, font_thickness, cv2.LINE_AA)                
+                if args.display_dot:
+                    _x = x1+x2 // 2
+                    _y = y1+y2 // 2
+                    cv2.circle(img_numpy,
+                        center=(_x, _y),
+                        radius=30,
+                        color=color,
+                        thickness=-1,
+                        lineType=cv2.LINE_4,
+                        shift=0
+                    )
+
 
             if args.display_text:
                 _class = cfg.dataset.class_names[classes[j]]
