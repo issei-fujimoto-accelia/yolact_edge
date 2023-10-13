cbb729bef24418a7e476aef5ed5f5ea6eeeea8ab
accelia <accelia@accelia.net>
2023-10-13 17:53:12 +0900

    add train shell for run local

--- run_train.sh
+++ run_train.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-python3 train.py \
+python train.py \
 --config=turnip_mobilenetv2_config \
 --resume="./pre_weights/yolact_edge_mobilenetv2_54_800000.pth" \
---start_iter=-1 \
+--start_iter=0 \
 --batch_size=4 \
 --num_workers=0 \
 --lr=0.001 \
