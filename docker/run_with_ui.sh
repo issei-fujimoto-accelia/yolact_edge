59cae1e0b8a9069198779dfa253879e379468bdd
accelia <accelia@accelia.net>
2025-10-31 17:22:33 +0900

    add docker

--- docker/run_with_ui.sh
+++ docker/run_with_ui.sh
@@ -9,7 +9,9 @@ docker run --rm --gpus all -it --name=yolact_edge \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
   -v $SOURCE_CODE:/yolact_edge/:rw \
   -v /usr/share/fonts:/usr/share/fonts \
-  --device /dev/video2:/dev/video0:mwr \
+  --device /dev/video0:/dev/video0:mwr \
+  --device /dev/video1:/dev/video1:mwr \
+  --device /dev/video2:/dev/video2:mwr \
   -e DISPLAY=$DISPLAY \
-  yolact_edge_image \  
+  yolact_edge_image \
   python run_with_window.py
