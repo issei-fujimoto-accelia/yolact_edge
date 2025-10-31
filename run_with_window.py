ed40424f6d2a7459ee87b62ae03f38d6e505f053
accelia <accelia@accelia.net>
2025-10-31 15:36:51 +0900

    add ui

--- run_with_window.py
+++ run_with_window.py
@@ -1,9 +1,4 @@
 """
-4点の指定の座標がずれている
-縦横を１とした座標とかにしたほうが良いかも
-
-
-
 """
 
 
@@ -11,8 +6,11 @@ from eval import run
 import tkinter as tk
 from ui import CameraApp
 import multiprocessing
+import time
+from screeninfo import get_monitors
 
-weight_path="./weights/yolact_edge_resnet101_244_1711_v8.pth"
+
+weight_path="/home/accelia/i.fujimoto/yolact_edge/weights/yolact_edge_resnet101_244_1711_v8.pth"
 args = [
     "--score_threshold=0.7",
     f"--trained_model={weight_path}",
@@ -23,31 +21,42 @@ args = [
     "--video_multiframe=1",
     "--use_fp16_tensorrt",
     "--only_turnip=true",
-    "--video=2",
+    "--video=0",
     "--disable_tensorrt",
     "--display_ajuster=false"
 ]
 
 def main():
+    monitors = get_monitors()
+    for i, m in enumerate(monitors):
+        print(f"{i}: {m}")
+
+    target = monitors[0]
+
     root = tk.Tk()
+    root.geometry(f"{target.width}x{target.height}+{target.x}+{target.y}")
+    # root.attributes("-fullscreen", True)
+
     app = CameraApp(root)
 
     event = multiprocessing.Event()
-    process = multiprocessing.Process(target=run, args=(args, app.set_frame, event, app.sizeArray, app.colorArray, app.pointArray))
+    # multiprocessing.set_start_method("spawn", force=True)
+    process = multiprocessing.Process(target=run, args=(args, app.set_frame, event, app.sizeArray, app.colorArray, app.pointArray,))
     process.start()
 
     def close():
         process.terminate()
         app.stop()
 
+    ## windowのclose buttonのcallback
     root.protocol("WM_DELETE_WINDOW", close)
-    root.mainloop()
-    process.join()
 
     if event.is_set():
+        print("event is set()")
         close()
 
-    # run(args, app.set_frame)
-
+    root.mainloop()
+    process.join()
+    
 if __name__ == '__main__':
     main()
